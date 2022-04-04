5fac158a7aae9532bb291bd5e7db9b53c053f5db
SANDQVISTT <jonathan.sandqvist@gritacademy.se>
2022-04-04 17:57:24 +0200

    [ADD] Funktion för att välja profilbild när man reggar användare.

--- src/modules/profile.ts
+++ src/modules/profile.ts
@@ -1,28 +1,19 @@
 import { db } from "./firebaseApp";
 import { onValue, ref, get } from "firebase/database";
 
-/* const dbReff = ref(db, '/users/userInfo');
- onValue(dbReff, snapshot=>{
-    const data = snapshot.val();
-    console.log(data)
 
- }) */
-
-// const edizRef = ref(db, '/users/userInfo/edzone')
-// get(edizRef).then(snapshot => {
-    
-//     console.log(snapshot.val());
-// })
-//Hämtar användernamn och kön ifrån Main.ts och sätter ut på profil-sidan
 let usernameID = sessionStorage.getItem("user");
 let genderID = sessionStorage.getItem("gender");
 let bioID = sessionStorage.getItem("bio");
+let picID = sessionStorage.getItem("pic");
 
-console.log(usernameID, genderID, bioID)
+console.log(picID,"testar att få bildens URL")
 
+//
 const namn:HTMLElement = document.querySelector('#UsernameID')
 const gender: HTMLElement = document.querySelector('#genderID');
 const bio: HTMLElement = document.querySelector('#bioID');
+//
 bio.innerText = bioID;
 gender.innerText = genderID;
 namn.innerText = usernameID;
