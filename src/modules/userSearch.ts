644c067eb7a6a9f6aa8f66bdcef479c458df8a72
J0kk3 <joakim_88@msn.com>
2022-04-04 12:48:48 +0200

    testcode

--- src/modules/userSearch.ts
+++ src/modules/userSearch.ts
@@ -7,14 +7,15 @@ document.getElementById("userSelect").onclick = function ()
 {
     let values = [];
     values.push(dbRef);
-    // let userNames = ref(db, "/users/userInfo/"+ "username") //Get users from the database
+    let userNames = ref(db, "/users/userInfo/".search("username").toString()) //Get users from the database
 
     for (const val of values)
     {
         let userSelect = document.getElementById("userSelect");
         let option = document.createElement("option");
         option.value = val;
-        option.text = val.charAt(0).toUpperCase() + values.slice(1);
+        // option.text = val.charAt(0).toUpperCase() + values.slice(1);
+        // option.text = userNames.toString();
         userSelect.appendChild(option);
     }
 }
