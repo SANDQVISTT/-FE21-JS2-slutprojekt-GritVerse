29ea9fed5ae065988c5a54bf1e541321875336b4
J0kk3 <joakim_88@msn.com>
2022-04-04 10:59:18 +0200

    [ADD] select to home and initial code

---
+++ src/modules/userSearch.ts
@@ -0,0 +1,21 @@
+import { ref } from "firebase/database";
+import { db } from "./firebaseApp";
+
+const dbRef = ref(db, "/users/userInfo/");
+
+document.getElementById("userSelect").onclick = function ()
+{
+    let values = [];
+    values.push(dbRef);
+    // let userNames = ref(db, "/users/userInfo/"+ "username") //Get users from the database
+
+    for (const val of values)
+    {
+        let userSelect = document.getElementById("userSelect");
+        let option = document.createElement("option");
+        option.value = val;
+        option.text = val.charAt(0).toUpperCase() + values.slice(1);
+        userSelect.appendChild(option);
+    }
+}
+let userSelect = document.getElementById("userSelect");
