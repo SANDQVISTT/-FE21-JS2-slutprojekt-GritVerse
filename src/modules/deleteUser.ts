60b7fac489348a376738186a9c9c8838768eef6d
edzonee <edzone1996@gmail.com>
2022-04-05 22:52:57 +0200

    delete your own profile only from your own page

--- src/modules/deleteUser.ts
+++ src/modules/deleteUser.ts
@@ -1,39 +1,35 @@
 import { ref, remove } from "firebase/database";
 import { db } from "./firebaseApp";
 
-/* const currentUser = new UserSign(); */
-
+/* Function for deleting your own profile */
 export function deleteProfile(): void {
   const profileDIV = document.getElementById("ProfileInfo") as HTMLInputElement;
-  const deleteProfileBtn = document.createElement(
-    "button"
+  const deleteProfileBtn = document.getElementById(
+    "delete-account"
   ) as HTMLButtonElement;
-  profileDIV.append(deleteProfileBtn);
-  deleteProfileBtn.innerText = "Delete profile";
-  console.log(sessionStorage.getItem("user"));
-  deleteProfileBtn.addEventListener("click", (e) => {
-    let deleteAccountBtn = confirm("Are you sure?");
-    if (deleteAccountBtn) {
-      const deleteUser = ref(
-        db,
-        "/users/userInfo/" + sessionStorage.getItem("user")
-      );
-      sessionStorage.clear();
-      remove(deleteUser);
-      window.location.href = "/index.html";
-    }
-  });
+  deleteProfileBtn.style.display = "none";
+  
+  /* temporary solution */
+  let usernameID = sessionStorage.getItem("user");
+  let target = sessionStorage.getItem("targetUser");
+  let currentUser = target === null ? usernameID : target;
 
-  /* console.log(currentUser) */
-  /* const dbRef = ref(getDatabase());
-  get(child(dbRef, `users/userInfo/${currentUser.username}`)).then((snapshot) => {
-    console.log(snapshot.val(), snapshot.exists());
-
-    if (sessionStorage.getItem("user") == ) {
-      deleteProfileBtn.innerText = "Delete profile";
-      profileDIV.append(deleteProfileBtn);
-      const deletePrf = ref(db, `/users/userInfo/${currentUser.username}`)
-      remove(deletePrf)
-    }
-  }); */
+  if (sessionStorage.getItem("user") == currentUser) {
+    deleteProfileBtn.style.display = "block";
+    console.log(sessionStorage.getItem("user"));
+    deleteProfileBtn.addEventListener("click", (e) => {
+      let deleteAccountBtn = confirm("Are you sure?");
+      if (deleteAccountBtn) {
+        const deleteUser = ref(
+          db,
+          "/users/userInfo/" + sessionStorage.getItem("user")
+        );
+        sessionStorage.clear();
+        remove(deleteUser);
+        window.location.href = "/index.html";
+      } else {
+        deleteProfileBtn.style.display = "none";
+      }
+    });
+  }
 }
